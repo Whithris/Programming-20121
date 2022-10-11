fbe3045a503e0f84a2d91fed33093849b229e512
Whithris <visualstudiouser7@gmail.com>
2022-10-12 02:17:18 +0300

    PR3-2

---
+++ PR3-1.py
@@ -0,0 +1,7 @@
+def main():
+    n = int(input())
+    data = input().split()
+    c = int(input())
+
+if __name__ == '__main__':
+    main()
---
+++ PR3-2.py
@@ -0,0 +1,27 @@
+def main():
+    data = input().split()
+    results = []
+    ns = len(data) #система счисления
+    variants = []
+    for i in range(ns**len(data)):
+        n = i
+        s = ''
+        while n > 0:
+            s += str(n%ns)
+            n //= ns
+        s = s[::-1]
+        while len(s) != len(data):
+            s = '0' + s
+        variants.append(s)
+    check = sorted(data)
+    for i in range(len(variants)):
+        s = ''
+        for j in range(len(variants[i])):
+            s += data[int(variants[i][j])]
+        if s not in results and sorted([x for x in s]) == check:
+            results.append(s)
+    print(results)
+
+if __name__ == '__main__':
+    main()
+
