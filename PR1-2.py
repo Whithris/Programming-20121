e67bbaf67dec824d1016beda5dd36b3e9cb19c93
Whithris <visualstudiouser7@gmail.com>
2022-10-19 11:28:34 +0300

    upd. PR1-2

--- PR1-2.py
+++ PR1-2.py
@@ -1,12 +1,9 @@
 def main():
     a = input()
-    if -2**7 < int(a) < 2**7 - 1:
-        if int(a) < 0:
-            if -2**7 < str(int(a)*-1)[::-1] < 2**7 - 1:
-                print(str(int(a)*-1)[::-1])
-        else:
-            if -2**7 < int(a[::-1]) < 2**7 - 1:
-                print(a[::-1])
+    if (0 <= int(a) < (2**7) - 1) and (int(a[::-1]) < (2**7) - 1):
+        print(int(a[::-1]))
+    elif -2**7 < int(a) < 0 and (int(a[0] + str(int(a[1:][::-1]))) > -2**7):
+        print((int(a[0] + str(int(a[1:][::-1])))))
     else:
         print('no solution')
 
